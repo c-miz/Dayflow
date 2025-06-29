ed4f2e173c2f62caaf55f52fb15abd06c5b422b3
c-miz <2400013167@stu.pku.edu.cn>
2025-06-29 21:52:30 +0800

    Delete scheduleitem.cpp

--- scheduleitem.cpp
+++
@@ -1,8 +0,0 @@
-#include "ScheduleItem.h"
-
-ScheduleItem::ScheduleItem(QString title, QTime start, QTime end, QDate d, RepeatType repeat, QStringList tags,int priority,int adAm) // 添加 tags,priority
-    : title(title), startTime(start), endTime(end), date(d), repeatType(repeat), tags(tags),priority(priority),advanceAmount(adAm) {} // 初始化 tags,priority
-
-bool ScheduleItem::isValid() const {
-    return startTime < endTime;
-}
